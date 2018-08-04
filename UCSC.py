155e7aa7bd762e3eba1ffa25d9daa60987852fa4
yuhsunchien <ru03vmp@gmail.com>
2018-08-04 10:02:51 -0700

    OK

--- UCSC.py
+++ UCSC.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import csv
+import re
+
 class_url='https://www.ucsc-extension.edu/certificate-program'
 ucsc_url='https://www.ucsc-extension.edu/'
 #Get html
@@ -16,11 +18,7 @@ def get_web_page(url):
 #Parse the courselist
 def parse1(dom):
     soup = BeautifulSoup(dom, 'html5lib')
-    # groups=soup.find('h3')
-    # print(groups)
     Allist = dict()
-    courselist=[]
-    courseurllist=[]
     courses=soup.find_all('div','views-field views-field-name')
     for course in courses:
         Allist.update({course.text.strip():course.a['href']})
@@ -30,6 +28,7 @@ def parse2(dom,key):
     soup = BeautifulSoup(dom, 'html5lib')
     links=soup.find_all('div','post-content')
     courselists=[]
+
     try:
         nextpage=soup.find('a',title='Go to next page')['href']
     except:
@@ -57,10 +56,16 @@ def parse2(dom,key):
                         # for tempp1 in tempp:
                         #     if tempp1.find_all('a') and re.match(r'/shop.*', tempp1.find('a')['href']):
                         web=tempp[7].find('a')['href']
-                    #https: // www.ucsc - extension.edu / full - schedule - by - section?section_id = 6004351 & width = 700 & iframe = true
+
                         date_temp=date.split('/')
                         week_temp=datetime(int(date_temp[2].strip()),int(date_temp[0].strip()),int(date_temp[1].strip())).weekday()+1
                         week=weekday(str(week_temp))
+
+                        temp = re.split('=|&', web)
+                        href = "http://course.ucsc-extension.edu/modules/shop/defaultSections.action"
+                        period, Credits = parse3(href, temp[3], temp[5])
+
+
                         courselists.append({
                             'CourseNumber': number,
                             'StartDate': date,
@@ -71,12 +76,35 @@ def parse2(dom,key):
                             'href':web,
                             'name': name.text.strip(),
                             'group':key,
-                            'time':time
+                            'time':time,
+                            'period':period,
+                            'Credits':Credits
 
                                             })
+
+
                     except:
                         pass
     return courselists,nextpage
+
+
+def parse3(url,offID,SecID):
+    def post_web_page(url,postdata):
+        resp = requests.post(
+            url=url,data=postdata)
+        if resp.status_code != 200:
+            #print('Invalid url:', resp.url)
+            return None
+        else:
+            return resp.text
+    #Parse the courselist
+    dom = post_web_page(url,{"OfferingID":offID,"SectionID":SecID})
+    soup = BeautifulSoup(dom, 'html.parser')
+    enddate=soup.find("enddate")
+    startdate=soup.find("startdate")
+    Credit = soup.find("credithours").text
+    period = str(startdate.text[:11]).strip()+"-"+str(enddate.text[:11]).strip()
+    return period,Credit
 def weekday(num):
     if num=='1':
         return 'Mon'
@@ -92,6 +120,10 @@ def weekday(num):
         return 'Sat'
     elif num == '7':
         return 'Sun'
+
+
+
+
 if __name__ == '__main__':
     print('Processing...........')
     #Grab groups list
@@ -102,31 +134,33 @@ if __name__ == '__main__':
     Allist=parse1(resp)
    #get every course detal
     for k in Allist.keys():
-        key=k
         newWeb=get_web_page(ucsc_url+Allist[k])
-        grouplist,nextpage=parse2(newWeb,key)
+        grouplist,nextpage=parse2(newWeb,k)
         while grouplist:
             #To check nextpage bottom => Y:keep get next-page information.
             courselistsa.append(grouplist)
             count=count+1
-            print("imported " + str(count) + " pages of courses", end='\r')
+            print("imported " + str(count) + " pages of courses")
             try:
+                newWeb = get_web_page(ucsc_url + nextpage)
                 print ()
                 if newWeb==None:
                     break
                 else:
-                    grouplist, nextpage = parse2(newWeb, key)
+                    grouplist, nextpage = parse2(newWeb, k)
             except:
                 grouplist=[]
-    #Print csv
+
+
+#Print csv
     with open('List.csv', 'w', encoding='utf-8', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(('Group', 'Name', 'CourseNumber', 'StartDate', 'Day','Time',
+        writer.writerow(('Group', 'Name', 'CourseNumber', 'StartDate', 'Days','Time',"Period",'Credits',
                          'Campus', 'Fee', 'Teacher', 'Detail'))
         for groups in courselistsa:
             for listsa in groups:
                 writer.writerow((listsa['group'], listsa['name'], listsa['CourseNumber'], listsa['StartDate'], listsa['Day'], listsa['time'],
-                                 listsa['Campus'], listsa['fee'], listsa['teacher'], listsa['href']))
+                                 listsa["period"],listsa["Credits"],listsa['Campus'], listsa['fee'], listsa['teacher'], listsa['href']))
     print('Done!!Press any key to exit')
     input()
 
